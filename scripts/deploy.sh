b4a0106cb734376801e6c67ef5187eed15620a9c
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-22 17:26:07 -0600

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 eval "$(ssh-agent -s)"
-chmod 600 .travis/deploy.key
-ssh-add .travis/deploy.key
+chmod 600 ../.travis/deploy.key
+ssh-add ../.travis/deploy.key
 ssh-keyscan kvstage.cloud >> ~/.ssh/known_hosts
 git remote add deploy dokku@kvstage.cloud:holdopen
 git config --global push.default simple
