c932162b7f3860832e043096f0304d5b622bab31
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-22 17:31:22 -0600

    Move before install into deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 eval "$(ssh-agent -s)"
+openssl aes-256-cbc -K $encrypted_ea482d0fbc94_key -iv $encrypted_ea482d0fbc94_iv -in .travis/deploy.key.enc -out .travis/deploy.key -d
 chmod 600 ./.travis/deploy.key
 ssh-add ./.travis/deploy.key
 ssh-keyscan kvstage.cloud >> ~/.ssh/known_hosts
