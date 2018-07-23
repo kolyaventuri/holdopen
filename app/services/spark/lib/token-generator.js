6851d491c4cdf45463757b32695c375cec8573cb
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-22 20:17:57 -0600

    Add token persistence

--- app/services/spark/lib/token-generator.js
+++ app/services/spark/lib/token-generator.js
@@ -16,19 +16,25 @@ class TokenGenerator {
     this.secret = secret;
 
     this.API_SIG = crypto.createHash('md5').update(secret + "ApiKey" + key).digest("hex");
+
+    this.token = {expires: new Date(0)};
   }
 
   async getToken() {
+    if(this.token.expires > new Date()) return this.token;
+    
     let result = await API.post('/session', { ApiKey: this.key, ApiSig: this.API_SIG });
 
     if(!result['D'].Success) {
       throw new Error('Something went wrong authorizing application.');
     }
 
-    return {
+    this.token = {
       token:   result.D.Results[0].AuthToken,
       expires: new Date(result.D.Results[0].Expires)
     };
+
+    return this.token;
   }
 }
 
