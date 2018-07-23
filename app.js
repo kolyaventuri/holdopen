238a9991eb567b203ce5cafb72e5767c817c8e5e
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-23 02:08:46 -0600

    Fix mongoose connection error

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('./db/mongo');
+
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 
--- app/controllers/api/v1/listings-controller.js
+++ app/controllers/api/v1/listings-controller.js
@@ -1,6 +1,18 @@
+const Listing = require('../../../models/listing');
+
+let LIMIT = 10;
+
 class ListingsController {
   static index(req, res, next) {
-    res.send('');
+    let skip = (req.query.page - 1) * LIMIT;
+
+    Listing.find(null, null, { skip, limit: LIMIT }).then((results) => {
+      res.json(results);
+    }).catch(err => {
+      console.error(err);
+      res.send().status(500);
+    });
+
   }
 }
 
