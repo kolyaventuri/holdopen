f7b2ad8146f90bcf6e5e728144ae075f81e09e68
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-23 02:26:04 -0600

    Serializer spec

---
+++ spec/modules/serializer/serializer.spec.js
@@ -0,0 +1,23 @@
+const Serializer = require('../../../lib/serializer');
+
+const attributeMap = {
+  'foo': 'otherFoo'
+};
+
+const serializer = new Serializer(attributeMap);
+
+describe('Serializer', () => {
+  it('can serialize an object', () => {
+    const object = {
+      foo: 'data',
+      bar: 'more data'
+    };
+
+    const serialized = serializer.serialize(object);
+
+    expect(serialized).to.not.have.property('foo');
+    expect(serialized).to.not.have.property('bar');
+
+    expect(serialized).to.have.property('otherFoo').that.eqls(object.foo);
+  });
+})
