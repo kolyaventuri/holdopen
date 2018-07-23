98343abb15e8b1627a5540d14acd096dace7039d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-23 00:16:40 -0600

    Framework the cli tool for pulling homes

--- tasks/spark/pull-homes.js
+++ tasks/spark/pull-homes.js
@@ -1,1 +1,45 @@
-console.log('TODO: Implement')
+const args = require('gar')(process.argv.slice(2));
+const readline = require('readline');
+
+const color = {
+  reset: "\x1b[0m",
+  red: "\x1b[31m",
+  bgWhite: "\x1b[47m"
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let number = args.n || args.number || args.num;
+let force = !!args.f || !!args.force;
+
+const doRun = () => {
+  if(number && !Number.isFinite(number)) {
+    console.error(new Error('Provided value must be a number.'));
+    process.exit(1);
+  }
+
+  console.log('Lets go!');
+
+  process.exit(0);
+};
+
+if(!number && !force) {
+  console.log('This action will pull ALL homes from the MLS.');
+  console.log(`${color.bgWhite}${color.red}This is a costly action.${color.reset}`);
+  console.log('If you meant to pull a specific number, pass the -n flag');
+  rl.question(`Are you sure you want to proceed? [y/N]: `, (answer) => {
+    answer = answer.toLowerCase();
+
+    if(answer !== 'y' && answer !== 'yes') {
+      process.exit(0);
+      return;
+    }
+
+    doRun();
+  });
+} else {
+  doRun();
+}
