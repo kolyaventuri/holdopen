4d21b6194ecf7a1d5b9d54d6cff8e2f880f0fa4e
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-24 13:34:49 -0600

    Search spec

---
+++ spec/requests/api/v1/search/search.spec.js
@@ -0,0 +1,41 @@
+describe('A GET request to /api/v1/listings', () => {
+  let firstHome = null;
+
+  describe('with a zip code parameter', () => {
+    it('returns the MLS search results for that zip code', () => {
+      chai.request(app)
+        .get('/api/v1/search?q=87114')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+
+          expect(res.body).to.be.an('object');
+
+          let body = res.body;
+
+          let results = body.results;
+
+          exepect(results).to.be.an('array');
+
+          expect(results[0]).to.be.an('object');
+
+          firstHome = results[0];
+        });
+    });
+  });
+
+  describe('with a different zip code', () => {
+    it('should return different homes', () => {
+      chai.request(app)
+        .get('/api/v1/search?q=87120')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+
+          let home = res.body.results[0];
+
+          expect(home).to.not.eql(firstHome);
+        });
+    });
+  });
+});
