407673bf527949626596b354edb78ee92cda364e
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-24 14:03:57 -0600

    Add zip code parsing

--- app/services/spark/lib/query-parser.js
+++ app/services/spark/lib/query-parser.js
@@ -1,6 +1,11 @@
+const zipRegex = /^\b\d{5}(-\d{4})?\b$/;
+
 class QueryParser {
   static parse(query) {
-    
+    let isZip = zipRegex.test(query);
+    if(isZip) {
+      return { zip: query };
+    }
   }
 }
 
