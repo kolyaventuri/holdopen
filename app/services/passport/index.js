7eb55fd612d2aad2488fd6fdcb4f37c82753798e
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-29 12:42:40 -0600

    User profile creation

--- app/services/passport/index.js
+++ app/services/passport/index.js
@@ -1,14 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
+const User = require('../../models/user');
+
 module.exports = (app) => {
   passport.use(new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL
-    }, (accessToken, refreshToken, profile, cb) => {
-      return cb(null, profile);
-    });
+    }, async (accessToken, refreshToken, profile, cb) => {
+      let user = null;
+      try {
+        user = await User.findOne( { id: profile.id });
+
+        if(user) return cb(null, user);
+
+        user = await User.create(profile);
+
+        return cb(null, user);
+      } catch (err) {
+        return cb(err);
+      }
+    })
   );
 
   app.use(passport.initialize());
