343d6360f129468f426db88a762d2517edae2b50
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-30 23:17:06 -0600

    Search API requires authentication

--- routes/api/v1/search/index.js
+++ routes/api/v1/search/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const SearchController = require('../../../../app/controllers/api/v1/search-controller');
 
+const loggedIn = require('../../../../app/helpers/logged-in/hard-fail');
+
+router.use('*', loggedIn);
+
 router.get('/', SearchController.index);
 
 module.exports = router;
