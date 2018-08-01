c2a51c04dffbd72d2d30d0c8681bcc081823cba6
Kolya Venturi <kolya.venturi@enx3s.com>
2018-08-01 04:28:58 -0600

    Rewrite google maps component to pull in open homes

--- app/assets/react/components/App.jsx
+++ app/assets/react/components/App.jsx
@@ -4,15 +4,13 @@ import Map from './Map.jsx';
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            markers: []
+        }
     }
     render() {
         return (
-            <Map
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDN532AwH9GbcNurROKbFG01SjXnXNB3f0"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px`, width: `100%` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-            />
+            <Map />
         )
     }
 }
